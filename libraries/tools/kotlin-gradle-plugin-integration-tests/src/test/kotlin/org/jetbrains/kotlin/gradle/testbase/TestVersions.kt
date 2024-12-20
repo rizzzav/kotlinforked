ac3ade06855b481feec3e278138830066a6a32c8
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2024-12-20 17:38:58 +0100

    [Gradle] Update to final 8.12 release

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/testbase/TestVersions.kt
@@ -29,7 +29,7 @@ interface TestVersions {
         const val G_8_9 = "8.9"
         const val G_8_10 = "8.10.2"
         const val G_8_11 = "8.11.1"
-        const val G_8_12 = "8.12-rc-1"
+        const val G_8_12 = "8.12"
 
         /**
          * Check [org.jetbrains.kotlin.gradle.GradleCompatibilityIT.testIncompatibleGradleVersion]
