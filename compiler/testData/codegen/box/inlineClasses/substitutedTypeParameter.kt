5d1c3c0204ff55052d64b8cd64f073c664111f2a
Alejandro Serrano Mena <alejandro.serrano.mena@jetbrains.com>
2025-01-31 17:23:10 +0100

    Make test more resilient

--- compiler/testData/codegen/box/inlineClasses/substitutedTypeParameter.kt
+++ compiler/testData/codegen/box/inlineClasses/substitutedTypeParameter.kt
@@ -24,14 +24,19 @@ fun box(): String {
         fooInt.quux(fooString)
     }
 
+    val wrapperClass = Wrapper::class.java
     val exampleClass = Example::class.java
     val methods = exampleClass.getDeclaredMethods()
 
     val barMethod = methods.single { it.name.startsWith("bar") }
-    require(barMethod.toGenericString() == "public static final <R> void Example.${barMethod.name}(Wrapper<java.lang.Integer>,Wrapper<R>)")
+    require(barMethod.toGenericString() == "public static final <R> void ${exampleClass.name}.${barMethod.name}(${wrapperClass.name}<java.lang.Integer>,${wrapperClass.name}<R>)") {
+        "Unexpected method signature for 'bar': ${barMethod.toGenericString()}"
+    }
 
     val quuxMethod = methods.single { it.name.startsWith("quux") }
-    require(quuxMethod.toGenericString() == "public static final void Example.${quuxMethod.name}(Wrapper<java.lang.Integer>,Wrapper<java.lang.String>)")
+    require(quuxMethod.toGenericString() == "public static final void ${exampleClass.name}.${quuxMethod.name}(${wrapperClass.name}<java.lang.Integer>,${wrapperClass.name}<java.lang.String>)") {
+        "Unexpected method signature for 'quux': ${quuxMethod.toGenericString()}"
+    }
 
     return "OK"
 }
