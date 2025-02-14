69c8260cb0db73b025f1867d01e0d26b7b7bafd1
Vladimir Iakunin <vladimir.iakunin@jetbrains.com>
2025-02-14 16:30:29 +0100

    [Gradle] Make the KotlinGradleIT.noAttributesLegacyConfigurations test CC compatible
    ^KT-67291 In Progress

--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/kotlin/org/jetbrains/kotlin/gradle/KotlinGradlePluginIT.kt
@@ -801,7 +801,6 @@ class KotlinGradleIT : KGPBaseTest() {
 
     @DisplayName("KT-51913: KGP should not add attributes to 'legacy' configurations")
     @GradleTest
-    @BrokenOnMacosTest
     fun noAttributesLegacyConfigurations(gradleVersion: GradleVersion) {
         project(
             "legacyConfigurationConsumer",
--- libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/legacyConfigurationConsumer/producer/build.gradle.kts
+++ libraries/tools/kotlin-gradle-plugin-integration-tests/src/test/resources/testProject/legacyConfigurationConsumer/producer/build.gradle.kts
@@ -2,12 +2,12 @@ plugins {
     id("base")
 }
 
-val publishedFile = layout.buildDirectory.file("publication/output.txt")
-
 val producerTask by tasks.registering {
+    inputs.property("projectName",  project.name)
+    val publishedFile = layout.buildDirectory.file("publication/output.txt")
     outputs.file(publishedFile)
     doLast {
-        publishedFile.get().asFile.writeText("Hello World from ${project.name}")
+        publishedFile.get().asFile.writeText("Hello World from ${inputs.properties["projectName"]}")
     }
 }
 
