6dfb203f95fb33d5ac08a2496e5853be61a15a63
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2025-02-19 19:03:03 +0200

    fixup! [FIR] Unify `subject` and `subjectVariable` in when expressions
    Add a test to `analysis-api/testData/symbols/symbolByReference`.

---
+++ analysis/analysis-api/testData/symbols/symbolByReference/unstableSmartcastImplicitWhenSubject.kt
@@ -0,0 +1,16 @@
+// DO_NOT_CHECK_SYMBOL_RESTORE_K1
+// DO_NOT_CHECK_NON_PSI_SYMBOL_RESTORE_K2
+
+interface Ctx
+class CtxImpl : Ctx {
+    fun doJob(a: Int) {}
+    fun doJob(s: String) {}
+}
+
+open class Test(open val ctx: Ctx) {
+    fun test() {
+        when (ctx) {
+            is CtxImpl -> <caret>ctx.doJob(2)
+        }
+    }
+}
