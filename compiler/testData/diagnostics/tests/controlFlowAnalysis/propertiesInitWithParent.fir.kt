5cdfc8b073237b2e9d14c244f19f260fcb6afc00
Brian Norman <brian.norman@jetbrains.com>
2025-02-21 11:20:04 +0100

    fixup! [Test] Reproducer for missing val cannot be assigned (KT-74421)
    Add test cases involving substitution overrides.

--- compiler/testData/diagnostics/tests/controlFlowAnalysis/propertiesInitWithParent.fir.kt
+++ compiler/testData/diagnostics/tests/controlFlowAnalysis/propertiesInitWithParent.fir.kt
@@ -1,25 +1,68 @@
 // RUN_PIPELINE_TILL: FRONTEND
 // ISSUE: KT-74421
 
-open class Base {
+open class OpenBase {
     val x: String = "ok"
 }
 
-class InitViaSuper : Base() {
+class InitOpenViaSuper : OpenBase() {
     init {
         super.<!VAL_REASSIGNMENT!>x<!> = "error"
     }
 }
 
-class InitViaThis : Base() {
+class InitOpenViaThis : OpenBase() {
     init {
         this.x = "error"
     }
 }
 
-
-class InitViaImplicit : Base() {
+class InitOpenViaImplicit : OpenBase() {
     init {
         x = "error"
+    }
+}
+
+open class GenericOpenBase<T> {
+    val x: String = "ok"
+}
+
+class InitGenericOpenViaSuper : GenericOpenBase<String>() {
+    init {
+        super.<!VAL_REASSIGNMENT!>x<!> = "error"
+    }
+}
+
+class InitGenericOpenViaThis : GenericOpenBase<String>() {
+    init {
+        this.<!VAL_REASSIGNMENT!>x<!> = "error"
+    }
+}
+
+class InitGenericOpenViaImplicit : GenericOpenBase<String>() {
+    init {
+        <!VAL_REASSIGNMENT!>x<!> = "error"
+    }
+}
+
+interface InterfaceBase {
+    val x: String
+}
+
+class InitOpenAndInterfaceViaSuper : OpenBase(), InterfaceBase {
+    init {
+        super.<!VAL_REASSIGNMENT!>x<!> = "error"
+    }
+}
+
+class InitOpenAndInterfaceViaThis : OpenBase(), InterfaceBase {
+    init {
+        this.<!VAL_REASSIGNMENT!>x<!> = "error"
+    }
+}
+
+class InitOpenAndInterfaceViaImplicit : OpenBase(), InterfaceBase {
+    init {
+        <!VAL_REASSIGNMENT!>x<!> = "error"
     }
 }
--- compiler/testData/diagnostics/tests/controlFlowAnalysis/propertiesInitWithParent.kt
+++ compiler/testData/diagnostics/tests/controlFlowAnalysis/propertiesInitWithParent.kt
@@ -1,24 +1,67 @@
 // RUN_PIPELINE_TILL: FRONTEND
 // ISSUE: KT-74421
 
-open class Base {
+open class OpenBase {
     val x: String = "ok"
 }
 
-class InitViaSuper : Base() {
+class InitOpenViaSuper : OpenBase() {
     init {
         <!VAL_REASSIGNMENT!>super.x<!> = "error"
     }
 }
 
-class InitViaThis : Base() {
+class InitOpenViaThis : OpenBase() {
     init {
         <!VAL_REASSIGNMENT!>this.x<!> = "error"
     }
 }
 
+class InitOpenViaImplicit : OpenBase() {
+    init {
+        <!VAL_REASSIGNMENT!>x<!> = "error"
+    }
+}
 
-class InitViaImplicit : Base() {
+open class GenericOpenBase<T> {
+    val x: String = "ok"
+}
+
+class InitGenericOpenViaSuper : GenericOpenBase<String>() {
+    init {
+        <!VAL_REASSIGNMENT!>super.x<!> = "error"
+    }
+}
+
+class InitGenericOpenViaThis : GenericOpenBase<String>() {
+    init {
+        <!VAL_REASSIGNMENT!>this.x<!> = "error"
+    }
+}
+
+class InitGenericOpenViaImplicit : GenericOpenBase<String>() {
+    init {
+        <!VAL_REASSIGNMENT!>x<!> = "error"
+    }
+}
+
+interface InterfaceBase {
+    val x: String
+}
+
+class InitOpenAndInterfaceViaSuper : OpenBase(), InterfaceBase {
+    init {
+        <!VAL_REASSIGNMENT!>super.x<!> = "error"
+    }
+}
+
+class InitOpenAndInterfaceViaThis : OpenBase(), InterfaceBase {
+    init {
+        <!VAL_REASSIGNMENT!>this.x<!> = "error"
+    }
+}
+
+class InitOpenAndInterfaceViaImplicit : OpenBase(), InterfaceBase {
     init {
         <!VAL_REASSIGNMENT!>x<!> = "error"
     }
